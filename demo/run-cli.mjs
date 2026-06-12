6af5632acda8d8ad9e20b5a311f67bbab1d06c32
henderkes <m@pyc.ac>
2026-06-12 17:17:18 +0700

    load intl in example

--- demo/run-cli.mjs
+++ demo/run-cli.mjs
@@ -6,7 +6,7 @@ import phpBinary from '../build/php-cli.mjs';
 var bufferA = [];
 
 var loadPhp = async function () {
-	const { ccall } = await phpBinary({
+	const { ccall, FS } = await phpBinary({
 		print(data) {
 			if (!data) {
 				return;
@@ -17,7 +17,16 @@ var loadPhp = async function () {
 			}
 			bufferA.push(data);
 		},
+		preRun(module) {
+			module.ENV.PHPRC = '/php.ini';
+		},
 	});
+
+	FS.mkdir('/icu');
+	FS.writeFile('/icu/icudt74l.dat', fs.readFileSync(new URL('../build/icudt74l.dat', import.meta.url)));
+
+	FS.writeFile('/intl.so', fs.readFileSync(new URL('../build/intl.so', import.meta.url)));
+	FS.writeFile('/php.ini', 'extension_dir=/\nextension=intl\n');
 
 	var version = ccall("phpw_run", null, ["string"], ["?>" + fs.readFileSync(process.argv[2])]);
 
